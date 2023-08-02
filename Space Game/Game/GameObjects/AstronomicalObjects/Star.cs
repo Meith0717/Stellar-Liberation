67deb1ffdfdeed49bd1da11613ac555149fe64c7
Meith0717 <meith0717@icloud.com>
2023-08-02 16:14:05 +0200

    Add unsaved File

--- Space Game/Game/GameObjects/AstronomicalObjects/Star.cs
+++ Space Game/Game/GameObjects/AstronomicalObjects/Star.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CelestialOdyssey.Game.GameObjects
+namespace CelestialOdyssey.Game.GameObjects.AstronomicalObjects
 {
     internal class Star
     {
