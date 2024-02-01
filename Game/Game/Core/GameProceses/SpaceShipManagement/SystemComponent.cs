658cd80a233a9ac0691f0f45965497da6bfcc4e6
Thierry <meith0717@gmail.com>
2024-02-01 09:52:34 +0100

    Add Abstract SystemComponent Class #71

---
+++ Game/Game/Core/GameProceses/SpaceShipManagement/SystemComponent.cs
@@ -0,0 +1,19 @@
+﻿// SystemComponent.cs 
+// Copyright (c) 2023 Thierry Meiers 
+// All rights reserved.
+
+using Newtonsoft.Json;
+using System;
+
+namespace StellarLiberation.Game.Core.GameProceses.SpaceShipManagement
+{
+    [Serializable]
+    public abstract class SystemComponent
+    {
+        [JsonProperty] public int Level { get; private set; } = 1;
+
+        public void IncrementLevel() => Level++;
+
+        protected int GetLevelDependetValue(int value, float weightGreaterOne) => value * (int)Math.Pow(Level, 1 / weightGreaterOne);
+    }
+}
